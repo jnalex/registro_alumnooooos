84cfee1b1a029745101194a80ae69df85d8f49fc
unknown <sistemas@VRE_LAB0322.inacap.cl>
2022-07-13 12:02:15 -0400

    today

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://admind:admind123@registroalumnos.w65ro.mongodb.net/registroescuela?retryWrites=true&w=majority";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://admind:hola123@registroalumnos.w65ro.mongodb.net/registroescuela?retryWrites=true&w=majority";
